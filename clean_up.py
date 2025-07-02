d5da3f8ce14d411d815a7e0cf96ef99557d570f3
valevo <valevogelmann@gmail.com>
2025-07-02 23:57:52 +0200

    little restructure

--- clean_up.py
+++ clean_up.py
@@ -2,6 +2,8 @@ from glob import glob
 import yaml
 import json
 import re
+
+import os
 from pathlib import Path
 import shutil
 
@@ -19,20 +21,26 @@ shutil.rmtree("forKinsukAndSjors")
 shutil.rmtree("offline")
 shutil.rmtree("review")
 
+
+
 #############################################
 ### MOVE MARKDOWN FILES TO DOCS
 #############################################
 
-MD_DIR = "./EXPORTS/MD"
+MD_DIR = "./EXPORTS/WEBSITE"
 DOCS_DIR = "./docs"
+
+os.makedirs(DOCS_DIR)
 
 md_files = glob(MD_DIR + "/*/Dutch/*.yml") + glob(MD_DIR + "/*/English/*.yml")
 
 for f in md_files:
-    with open(f) as handle:
+    # with open(f) as handle:
         
     shutil.copy2(f, f.replace(MD_DIR, DOCS_DIR))
 
-shutil.rmtree("review")
 
+#############################################
+### 
+#############################################
 
---
+++ docs/setup_just-the-docs.py
@@ -0,0 +1,15 @@
+from glob import glob
+import yaml
+import json
+import re
+
+import os
+from pathlib import Path
+import shutil
+
+
+# place docs/ folder correctly
+
+# move docs/pages.yml to .github/workflows
+
+#
