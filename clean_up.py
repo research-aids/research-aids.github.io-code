6c4c131881ffbd03eaf89c79ab695603556b5c5b
valevo <valevogelmann@gmail.com>
2025-07-08 01:39:12 +0200

    safd

--- clean_up.py
+++ clean_up.py
@@ -33,6 +33,18 @@ MD_DIR = "./EXPORTS/WEBSITE"
 DOCS_DIR = "./docs"
 
 # os.makedirs(DOCS_DIR)
+niveau_files = glob(MD_DIR + "/*/*.md")
+for f in niveau_files:
+    # with open(f) as handle:
+    dest_fpath = f.replace(MD_DIR, DOCS_DIR)
+    print(f"copying {f} to {dest_fpath}")
+
+    os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
+    shutil.copy2(f, dest_fpath)
+
+
+
+
 
 md_files = glob(MD_DIR + "/*/Dutch/*.md") + glob(MD_DIR + "/*/English/*.md")
 
